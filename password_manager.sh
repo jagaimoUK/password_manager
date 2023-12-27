15ea9d71c72e21c23798d698a099258e42703649
jagaimo <kurosuke96382@gmail.com>
2023-12-27 14:49:16 +0900

    関数をリファクタリング

--- password_manager.sh
+++ password_manager.sh
@@ -1,8 +1,11 @@
 #!/bin/zsh
+# パスワードを追加する関数のの読み込み 
 source ./add_password.sh
+
+# 関数を定義
 function selection(){
-	echo '次の選択肢から入力してください(Add Password/Get Password/Exit)：';
-	read action;
+	echo '次の選択肢から入力してください(Add Password/Get Password/Exit)：'
+	read action
 }
 
 function get_password (){	
@@ -16,31 +19,31 @@ function get_password (){
 		echo サービス名：$service
 		echo ユーザー名：$user
 		echo パスワード名：$password
-
 	fi
 }
+# パスワードマネージャー
 echo 'パスワードマネージャーへようこそ！'
 
 while true
-do 
+do
+## 選択肢を入力	
 	selection
+## Add Passwordが入力された場合 
 	if [[ $action = "Add Password" ]]; then
 		add_password
 		gpg2 --yes -e -r jagaimo password.txt 
 		continue
-
-		
-# GetPassword が入力された場合
+## Get Passwordが入力された場合
 	elif [[ $action = 'Get Password' ]]; then
 		gpg2 --yes -r jagaimo -o password.txt -d password.txt.gpg
 		get_password 
 		continue
-# Exit が入力された場合
+## Exit が入力された場合
 	elif [[ $action = 'Exit' ]]; then 
 		echo 'Thank you!'
-	## プログラムが終了
+### プログラムが終了
 		break
-	# Add Password/Get Password/Exit 以外が入力された場合
+## Add Password/Get Password/Exit 以外が入力された場合
 	else  
 		echo '入力が間違えています。Add Password/Get Password/Exit から入力してください。'
 	fi
